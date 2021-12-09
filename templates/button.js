1a46fc1b7a2a6a258c2e0fda076cc9db5b03840a
tahara atsushi <a.tahara.sys20@morijyobi.ac.jp>
2021-12-09 10:21:51 +0900

    新しく作ったやつ

---
+++ templates/button.js
@@ -0,0 +1,8 @@
+document.getElementById('button').onclick = function () {
+    this.style.backgroundColor = "orange";
+  };
+
+  document.getElementById('button1').onclick = function () {
+    this.style.backgroundColor = "orange";
+
+  };
